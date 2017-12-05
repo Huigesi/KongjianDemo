424400811b41ca9766140713db9473e090f710a8
Huigesi <791339970@qq.com>
2017-12-05 14:22:36 +0800

    setError 错误警告

--- app/src/main/java/com/example/administrator/kongjiandemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/kongjiandemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.administrator.kongjiandemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -11,7 +13,9 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
     private ToggleButton toggleButton;
     private Switch aSwitch;
-    private RadioButton radioButton1,radioButton2;
+    private RadioButton radioButton1, radioButton2;
+    private Button button1;
+    private EditText editTextpsw, editTextname, editTextemail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,34 +26,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextname.getText().toString().trim();
+                String email=editTextemail.getText().toString().trim();
+                String psw=editTextpsw.getText().toString().trim();
+
+                //Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                //setError方法
+                if(name != null || name.equals("")){
+                    editTextname.setError("请输入用户名");
+                }else if(email != null || email.equals("")){
+                    editTextemail.setError("请输入邮箱");
+                }else if(psw != null || psw.equals("")){
+                    editTextpsw.setError("请输入密码");
+                }
+            }
+        });
 
     }
-    public void updateTB(View view){
-        boolean on=((ToggleButton) view).isChecked();
-        if(on){
-            Toast.makeText(this,"打开",Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this,"关闭",Toast.LENGTH_SHORT).show();
+
+    public void updateTB(View view) {
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+            Toast.makeText(this, "打开", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "关闭", Toast.LENGTH_SHORT).show();
         }
     }
-    public void updateSH(View view){
-        boolean on=((Switch) view).isChecked();
-        if(on){
-            Toast.makeText(this,"打开",Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this,"关闭",Toast.LENGTH_SHORT).show();
+
+    public void updateSH(View view) {
+        boolean on = ((Switch) view).isChecked();
+        if (on) {
+            Toast.makeText(this, "打开", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "关闭", Toast.LENGTH_SHORT).show();
         }
     }
-    public void updateRB(View view){
-        boolean on=((RadioButton) view).isChecked();
-        switch (view.getId()){
+
+    public void updateRB(View view) {
+        boolean on = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
             case R.id.radioButton:
-                if(on) {
+                if (on) {
                     Toast.makeText(this, "女", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.radioButton2:
-                if(on) {
+                if (on) {
                     Toast.makeText(this, "男", Toast.LENGTH_SHORT).show();
                 }
         }
@@ -58,10 +83,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initView() {
-        toggleButton=(ToggleButton)findViewById(R.id.toggleButton);
-        aSwitch=(Switch)findViewById(R.id.switch1);
-        radioButton1=(RadioButton)findViewById(R.id.radioButton);
-        radioButton1=(RadioButton)findViewById(R.id.radioButton2);
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        aSwitch = (Switch) findViewById(R.id.switch1);
+        radioButton1 = (RadioButton) findViewById(R.id.radioButton);
+        radioButton1 = (RadioButton) findViewById(R.id.radioButton2);
+        button1 = (Button) findViewById(R.id.button);
+        editTextemail = (EditText) findViewById(R.id.et_email);
+        editTextname = (EditText) findViewById(R.id.et_name);
+        editTextpsw = (EditText) findViewById(R.id.et_psw);
+
 
     }
 
