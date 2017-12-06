a4604c86534e6a3e86469faa519a66887849f138
Huigesi <791339970@qq.com>
2017-12-06 12:36:22 +0800

    SeekBar,progress值

--- app/src/main/java/com/example/administrator/kongjiandemo/TimePickerActivity.java
+++ app/src/main/java/com/example/administrator/kongjiandemo/TimePickerActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.kongjiandemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Chronometer;
+import android.widget.SeekBar;
 import android.widget.TextClock;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -11,6 +12,8 @@ public class TimePickerActivity extends AppCompatActivity {
     private TimePicker timePicker;
     private TextClock textClock;
     private Chronometer chronometer;
+    private SeekBar seekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +31,32 @@ public class TimePickerActivity extends AppCompatActivity {
                 Toast.makeText(TimePickerActivity.this,time,Toast.LENGTH_SHORT).show();
             }
         });
+        //设置刻度值
+        seekBar.setMax(100);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //刻度改变时
+                Toast.makeText(TimePickerActivity.this,progress+"",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //按下时
+                //Toast.makeText(TimePickerActivity.this,seekBar+"",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //放开时
+                //Toast.makeText(TimePickerActivity.this,seekBar+"",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initView() {
         timePicker=(TimePicker)findViewById(R.id.timePicker);
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
 
     }
 }
