b6f863387b35ec778b862ca5b3b0e32a70447bd6
Huigesi <791339970@qq.com>
2017-12-08 17:46:06 +0800

    ListView custom

---
+++ app/src/main/java/com/example/administrator/kongjiandemo/Itembean.java
@@ -0,0 +1,8 @@
+package com.example.administrator.kongjiandemo;
+
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+
+public class Itembean {
+}
--- app/src/main/java/com/example/administrator/kongjiandemo/ListViewActivity.java
+++ app/src/main/java/com/example/administrator/kongjiandemo/ListViewActivity.java
@@ -2,15 +2,18 @@ package com.example.administrator.kongjiandemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
     private ListView listView;
     private ArrayList<String> mList;
+    private Boolean is_divpage;
     private ArrayAdapter<String> adapter;
 
     @Override
@@ -27,6 +30,22 @@ public class ListViewActivity extends AppCompatActivity {
     private void initAdapter() {
         adapter=new ArrayAdapter(ListViewActivity.this,android.R.layout.simple_list_item_1,mList);
         listView.setAdapter(adapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (is_divpage&&scrollState== AbsListView.OnScrollListener.SCROLL_STATE_IDLE){
+                    //SCROLL_STATE_TOUCH_SCROLL手指没放开，到底就刷
+                    //SCROLL_STATE_IDLE手指放开就刷
+                    Toast.makeText(ListViewActivity.this,"gb",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                is_divpage=(firstVisibleItem+visibleItemCount==totalItemCount);
+
+            }
+        });
     }
 
     private void initData() {
