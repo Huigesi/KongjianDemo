fbb7a392bbbda3c19d315066b40dc61a34f992be
Huigesi <791339970@qq.com>
2017-12-08 10:50:30 +0800

    Toast custom

--- app/src/main/java/com/example/administrator/kongjiandemo/DialogActivity.java
+++ app/src/main/java/com/example/administrator/kongjiandemo/DialogActivity.java
@@ -6,12 +6,16 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -20,11 +24,11 @@ import java.util.Locale;
 
 public class DialogActivity extends AppCompatActivity {
 
-    private AlertDialog.Builder builder_custom,builder_single,builder_multi;
+    private AlertDialog.Builder builder_custom, builder_single, builder_multi;
     private DatePickerDialog date_dialog;
     private TimePickerDialog time_dialog;
-    private Button singleButton,MultiButton,customButton,DateButton,timeButton;
-    private int year, monthOfYear,day,hour,minute;
+    private Button singleButton, MultiButton, customButton, DateButton, timeButton;
+    private int year, monthOfYear, day, hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +41,11 @@ public class DialogActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        customButton=(Button)findViewById(R.id.button4);
-        singleButton=(Button)findViewById(R.id.button_single);
-        MultiButton=(Button)findViewById(R.id.button_multi);
-        DateButton=(Button)findViewById(R.id.button_date);
-        timeButton=(Button)findViewById(R.id.button_time);
+        customButton = (Button) findViewById(R.id.button4);
+        singleButton = (Button) findViewById(R.id.button_single);
+        MultiButton = (Button) findViewById(R.id.button_multi);
+        DateButton = (Button) findViewById(R.id.button_date);
+        timeButton = (Button) findViewById(R.id.button_time);
     }
 
     private void initDialog() {
@@ -85,7 +89,7 @@ public class DialogActivity extends AppCompatActivity {
         //单选
         builder_single = new AlertDialog.Builder(this);
         builder_single.setTitle("单选");
-        builder_single.setSingleChoiceItems(hobby,-1, new DialogInterface.OnClickListener() {
+        builder_single.setSingleChoiceItems(hobby, -1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
             }
@@ -107,24 +111,36 @@ public class DialogActivity extends AppCompatActivity {
             }
         });*/
         //日期对话框
-        Calendar calendar= Calendar.getInstance();
-        year=calendar.get(Calendar.YEAR);
-        monthOfYear=calendar.get(Calendar.MONTH);
-        day=calendar.get(Calendar.DAY_OF_MONTH);
-        date_dialog=new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        monthOfYear = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        date_dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                Toast.makeText(DialogActivity.this,year+"/"+(month+1)+"/"+dayOfMonth,Toast.LENGTH_SHORT).show();
+                //改变吐司位置
+                Toast toast = Toast.makeText(DialogActivity.this, year + "/" + (month + 1) + "/" + dayOfMonth, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP | Gravity.LEFT, 500, 300);
+                toast.show();
             }
         }, year, monthOfYear, day);
         date_dialog.setTitle("日期");
         //时间对话框
-        hour=calendar.get(Calendar.HOUR_OF_DAY);
-        minute=calendar.get(Calendar.MINUTE);
-        time_dialog=new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+        time_dialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                Toast.makeText(DialogActivity.this,+hourOfDay+":"+minute,Toast.LENGTH_SHORT).show();
+                //自定义toast
+                View view1 = LayoutInflater.from(DialogActivity.this).inflate(R.layout.toast_custom,
+                        (ViewGroup) findViewById(R.id.toast_layout_root));
+                ImageView image = (ImageView) view1.findViewById(R.id.image);
+                image.setImageResource(R.drawable.longface);
+                TextView text = (TextView) view1.findViewById(R.id.text);
+                text.setText(hourOfDay+":"+minute);
+                Toast toast=Toast.makeText(DialogActivity.this,text.toString(),Toast.LENGTH_SHORT);
+                toast.setView(view1);
+                toast.show();
             }
         }, hour, minute, true);
         time_dialog.setTitle("时间");
