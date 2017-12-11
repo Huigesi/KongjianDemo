29b6e95be9fdbc62bef0e2eecdbb2aae5cc592c0
Huigesi <791339970@qq.com>
2017-12-11 10:54:52 +0800

    menu

--- app/src/main/java/com/example/administrator/kongjiandemo/ButtonActivity.java
+++ app/src/main/java/com/example/administrator/kongjiandemo/ButtonActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.kongjiandemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class ButtonActivity extends AppCompatActivity {
@@ -10,6 +14,29 @@ public class ButtonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button);
         initView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.game_menu,menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.create_new:
+                Intent intent = new Intent(ButtonActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.help:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initView() {
